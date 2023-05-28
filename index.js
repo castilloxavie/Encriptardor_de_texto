67d3d17d6517f395355483dc7424f88f6349677b
castilloxavie <xavieralbertocastillovaron@hotmail.com>
2023-05-28 00:49:54 -0500

    Diseño de aplicacion Responsive desig y encriptacion de texto

---
+++ index.js
@@ -0,0 +1,27 @@
+function incriptar() {
+    let texto = document.getElementById('texto').value;
+    let tituloMensaje = document.getElementById('titulo_mensaje');
+    let parrafo = document.getElementById('parrafo');
+    let imagen = document.getElementById('munheco')
+
+    let texto_cifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+      
+      if (texto.length !=0){
+        document.getElementById('texto').value = texto_cifrado
+        tituloMensaje.textContent = 'texto encriptado exitosamente';
+        parrafo.textContent= "";
+        imagen.src = "./image/Codificacion.jpg";
+      }
+      else{
+        imagen.src = "./image/inspeccion.jpg";
+        tituloMensaje.textContent = "Ningun ,emsaje se  a encontrado"
+        parrafo.textContent = "Ingresa el texto que desa encriptar o desencriptar"
+        alert("debes de ingresar un texto")
+      }
+
+}
