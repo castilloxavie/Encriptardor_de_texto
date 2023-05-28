9661b3340a92ddd8fa3f533fd1d33f641d9b2d02
castilloxavie <xavieralbertocastillovaron@hotmail.com>
2023-05-28 01:05:38 -0500

    adicion de la funcionalidad desencriptado

--- index.js
+++ index.js
@@ -25,3 +25,31 @@ function incriptar() {
       }
 
 }
+
+function Desincriptar() {
+    let texto = document.getElementById('texto').value;
+    let tituloMensaje = document.getElementById('titulo_mensaje');
+    let parrafo = document.getElementById('parrafo');
+    let imagen = document.getElementById('munheco')
+
+    let texto_cifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+
+      if (texto.length !=0){
+        document.getElementById('texto').value = texto_cifrado
+        tituloMensaje.textContent = 'texto desencriptado exitosamente';
+        parrafo.textContent= "";
+        imagen.src = "./image/desincritar.jpg";
+      }
+      else{
+        imagen.src = "./image/inspeccion.jpg";
+        tituloMensaje.textContent = "Ningun ,emsaje se  a encontrado"
+        parrafo.textContent = "Ingresa el texto que desa encriptar o desencriptar"
+        alert("debes de ingresar un texto")
+      }
+      
+}
